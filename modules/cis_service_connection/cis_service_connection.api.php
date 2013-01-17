f78be262f800dbba6268767ae202cc472f4c368d
btopro <bto108@psu.edu>
2013-01-16 22:00:31 -0500

    added blocks for displaying section context specific content outlines, instructor contact information, as well as masquerade style section context override capability

--- modules/cis_service_connection/cis_service_connection.api.php
+++ modules/cis_service_connection/cis_service_connection.api.php
@@ -1,6 +1,9 @@
 <?php
 /**
  * Implements hook_set_cis_service_data().
+ * delta is a portion of the syncronization routine
+ * Common values are initial and interval
+ * New deltas can be added through custom code routines
  */
 function hook_set_cis_service_data($delta) {
   // only set this when the first run happens
@@ -9,4 +12,13 @@ function hook_set_cis_service_data($delta) {
       'field_in_cis_service_content_type' => 'value to set',
     );
   }
+}
+
+/**
+ * Implements hook_cis_section_list_alter().
+ * Allow for modification of the system reported section list
+ */
+function hook_cis_section_list_alter(&$sections) {
+  // add a section called default
+  $sections['default'] = t('Default');
 }
