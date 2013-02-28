2408e98d863ec71edf2b69b1fc2b4be6878bee4c
btopro <bto108@psu.edu>
2013-02-28 13:35:43 -0500

    better thumbnav support, feature repackaging, dropped rdf requirement

--- modules/cis_service_connection/js/cis_service_connection_inst_contact.js
+++ modules/cis_service_connection/js/cis_service_connection_inst_contact.js
@@ -2,11 +2,14 @@
   $(document).ready(function(){
     $('.thumbnav_controller img.cis_service_connection_inst_contact').click(function(){
       var text = '';
-      var title = $(this).attr('alt');
+      var title = '';
       if ($('#block-cis-service-connection-instructor-contact').length > 0) {
-        text += $('#block-cis-service-connection-instructor-contact .content').html();
+        text += $('#block-cis-service-connection-instructor-contact').html();
       }
       Drupal.thumbnav.modal(title, text);
     });
+		if ($('#block-cis-service-connection-instructor-contact').length == 0) {
+			$('.cis_service_connection_inst_contact').css('display', 'none');
+		}
   });
 })(jQuery);
