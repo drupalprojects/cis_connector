3abf976ac03528560022b8eb28102975271bd5fc
btopro <bto108@psu.edu>
2013-04-24 16:27:25 -0400

    Feature to connect any service to the ICOR service. includes textbook module integration for CKeditor plugin / button

---
+++ instance_data/cis_service_icor/plugins/icor/plugin.js
@@ -0,0 +1,21 @@
+﻿/*********************************************************************************************************/
+/**
+ * icor plugin for CKEditor 3.x
+ */
+/*********************************************************************************************************/
+
+CKEDITOR.plugins.add('icor',   
+  {    
+    requires: ['dialog'],
+    init:function(a) { 
+  var b="icor";
+  var c=a.addCommand(b,new CKEDITOR.dialogCommand(b));
+    c.modes={wysiwyg:1,source:0};
+    c.canUndo=false;
+  a.ui.addButton("icor",{
+          label:"Interactive Object",
+          command:b,
+          icon:this.path+"icor.png"
+  });
+  CKEDITOR.dialog.add(b,this.path+"dialogs/icor.js")}
+});
