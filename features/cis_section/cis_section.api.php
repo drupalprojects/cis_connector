8d5e4cfaac0f403025fe0aafb60ad6d18478efc0
btopro <bto108@psu.edu>
2013-08-05 09:02:01 -0400

    account creation abstracted from PSU module to handle that, activate / deactivate hooks reworked to be more flexible incase you don't like the connotation of add/drop

--- features/cis_section/cis_section.api.php
+++ features/cis_section/cis_section.api.php
@@ -1,12 +1,22 @@
 <?php
 /**
- * Implements hook_cis_section_activated().
+ * Implements hook_cis_section_activate().
  *
  * Allow for activing when a new section is activated.
  * this example is penn state's implementation to use a section being
  * active to talk to angel, our primary lms.. for now.
+ *
+ * Return is an array of callback functions to allow for easier
+ * alteration based on the context of the situation.
  */
-function hook_cis_section_activated($node) {
+function hook_section_activate() {
+  return array('_cis_section_activate_callback');
+}
+ 
+/**
+ * Example callback for activating a section
+ */
+function _cis_section_activate_callback($node) {
   // grab section id
   $section = array($node->field_section_id['und'][0]['value']);
   // pull the roster together for this section
@@ -18,10 +28,22 @@ function hook_cis_section_activated($node) {
 }
 
 /**
- * Implements hook_cis_section_deactivated().
+ * Implements hook_cis_section_deactivate().
  *
  * Allow for activing when a section is deactivated.
  */
-function hook_cis_section_deactivated($node) {
-  // write info back to cis offering about deactivation
+function hook_cis_section_deactivate() {
+  return array('deactivate_callback');
+}
+
+/**
+ * Implements hook_cis_section_user_insert_alter().
+ *
+ * Allow modification of the user object just prior to insert
+ * when the method used to create the user is section activation.
+ *
+ * This example shows how psu populates an email address automatically.
+ */
+function hook_cis_section_user_insert_alter(&$fields) {
+  $fields['email'] = _psu_utility_name_to_email($fields['name']);
 }
