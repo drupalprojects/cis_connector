5046dc332707ff46ad03960aed70d285ccfc0328
btopro <bto108@psu.edu>
2013-10-15 16:43:17 -0400

    shortcode API updated and working in an alpha state

--- modules/cis_shortcodes/cis_shortcodes.api.php
+++ modules/cis_shortcodes/cis_shortcodes.api.php
@@ -6,29 +6,44 @@
  * hence it is versioned for when newer connotations of data
  * to abstract are required.  Below are some example codes.
  */
+/*
+  [
+ 		cis={api version} *requried
+ 		tool={cis tool name} *required
+		item={tag or id} *required
+		render={iframe or link; default: iframe}
+		item_type={tag or id; default: id}
+		section={section to query, useful with tag; default: _user _context}
+		entity_type={entity type for the id; default: node}
+		entity_bundle={bundle for the id; default: NULL}
+		render_callback={function to handle values; default: NULL}
+	]
+
+*/
 /** 
  * Code 1 example
  * API version 1
  * connect to the associated MOOC distribution
- * pull resource (type assumed node) number 12
- * render as an iframe
+ * pull item (type assumed node) number 12
+ * render assumed as iframe
  */
-$code1 = '[cis=v1 bucket=mooc resource=12 render=iframe]';
+$code1 = '[cis=v1 tool=mooc item=12]';
 
 /** 
  * Code 2 example
  * API version 1
  * connect to the associated CLE distribution
- * pull the node resource tagged as "cool Stuff"
+ * pull the (assumed node) item tagged as "cool Stuff"
  * render as a link to the item
+ * Uses current user context to help calculate the node to deliver
  */
-$code2 = '[cis=v1 bucket=cle type=node tag="cool Stuff" render=link]';
+$code2 = '[cis=v1 tool=cle item="cool Stuff" item_type=tag render=link]';
 
 /**
  * Other examples
  */
 // default render style is iframe
-$code3 = '[cis=v1 bucket=media resource=12]';
-// request resource, grab output and embed into content
+$code3 = '[cis=v1 tool=media item=12]';
+// request item, grab output and embed into content
 // this is really only useful for images or non-responsive video
-$code4 = '[cis=v1 bucket=icor resource=16 render=embed]';
+$code4 = '[cis=v1 tool=icor item=16 render=embed]';
